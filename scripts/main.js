f9170576f6cbd257052b710ce138b73d41991bb3
cjgochanour <cjgochanour@gmail.com>
2021-12-02 15:43:20 -0600

    calculated price per order

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,5 @@ const renderAllHTML = () => {
 renderAllHTML();
 
 document.addEventListener("stateChanged", (event) => {
-    console.log("State of data has changed. Regenerating HTML...");
     renderAllHTML();
 });
